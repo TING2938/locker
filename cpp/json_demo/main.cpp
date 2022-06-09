80b91f936de81d07e23ec13a8e0849e12a9144fb
yeting <ye.ting@h3c.com>
2022-06-10 01:44:53 +0800

    add json parse/dump demo

--- cpp/json_demo/main.cpp
+++ cpp/json_demo/main.cpp
@@ -40,9 +40,52 @@ void test_json()
     std::cout << j4["AI_application_list"] << std::endl;
 }
 
+namespace struct_json1
+{
+struct Person
+{
+    std::string name;
+    std::string addr;
+    int age;
+};
+
+void to_json(json& j, const Person& p)
+{
+    j = json{{"name", p.name}, {"addr", p.addr}, {"age", p.age}};
+}
+
+void from_json(const json& j, Person& p)
+{
+    j.at("name").get_to(p.name);
+    j.at("addr").get_to(p.addr);
+    j.at("age").get_to(p.age);
+}
+}  // namespace struct_json1
+
+namespace struct_json2
+{
+struct Person
+{
+    std::string name;
+    std::string addr;
+    int age;
+
+public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(Person, name, addr, age);
+};
+
+// NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Person, name, addr, age);
+}  // namespace struct_json2
 
 int main(int, char**)
 {
     std::cout << "Hello, world!\n";
     test_json();
+
+
+    struct_json2::Person p{"Name", "Addr", 15};
+    json j = p;
+    std::cout << j << std::endl;
+    auto p2 = j.get<struct_json2::Person>();
+    assert(p.name == p2.name);
 }
