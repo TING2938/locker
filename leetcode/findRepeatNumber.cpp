0b65f9936f7aadf3b2dde6c455387565a2f4ccba
yeting <ye.ting@h3c.com>
2022-09-24 18:17:43 +0800

    add leetcode `findRepeatNumber`

---
+++ leetcode/findRepeatNumber.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+
+// url: https://leetcode.cn/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/?favorite=xb9nqhhg
+
+class Solution
+{
+public:
+    int findRepeatNumber(vector<int>& nums)
+    {
+        set<int> unique;
+        for (auto&& v : nums) {
+            if (unique.count(v)) {
+                return v;
+            } else {
+                unique.insert(v);
+            }
+        }
+        return 0;
+    }
+};
+
+TEST(leetcode, findRepeatNumber)
+{
+    Solution s;
+    vector<int> nums;
+
+    nums = {2, 3, 1, 0, 2, 5, 3};
+    EXPECT_EQ(s.findRepeatNumber(nums), 2);
+}
