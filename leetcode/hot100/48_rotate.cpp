eeb99e3b20dc7d25ad0f4c73bf3341e28e9c0c81
yeting <ye.ting@h3c.com>
2024-01-14 16:53:56 +0800

    add

---
+++ leetcode/hot100/48_rotate.cpp
@@ -0,0 +1,38 @@
+#include "list_utils.h"
+#include "pch.h"
+#include "vector_utils.h"
+
+// problem 48
+// url: https://leetcode.cn/problems/rotate-image/
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>>& matrix)
+    {
+        reverse(matrix.begin(), matrix.end());
+        int N = matrix.size();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
+
+TEST(leetcode, rotate)
+{
+    Solution s;
+
+    vector<vector<int>> matrix{
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
+
+    cout << matrix << endl;
+
+    s.rotate(matrix);
+
+    cout << matrix << endl;
+}
