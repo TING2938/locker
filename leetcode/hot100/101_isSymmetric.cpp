0bf2e4592c994640d51e5bf9dcca8d749eaa321e
yeting <yeting2938@163.com>
2024-01-17 15:11:49 +0800

    add

---
+++ leetcode/hot100/101_isSymmetric.cpp
@@ -0,0 +1,59 @@
+#include "list_utils.h"
+#include "pch.h"
+#include "tree_utils.h"
+#include "vector_utils.h"
+
+// problem 101
+// url: https://leetcode.cn/problems/symmetric-tree/
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+
+class Solution
+{
+public:
+    bool isSymmetric(TreeNode* root)
+    {
+        if (!root) {
+            return true;
+        }
+        return compare(root->left, root->right);
+    }
+
+    bool compare(TreeNode* left, TreeNode* right)
+    {
+        if ((left && !right) || (!left && right)) {
+            return false;
+        }
+        if (!left && !right) {
+            return true;
+        }
+        if (left->val != right->val) {
+            return false;
+        }
+        return compare(left->left, right->right) && compare(left->right, right->left);
+    }
+};
+
+TEST(leetcode, isSymmetric)
+{
+    Solution s;
+    auto tree = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)),
+                             new TreeNode(2, new TreeNode(5), new TreeNode(4)));
+    cout << tree << endl;
+    cout << s.isSymmetric(tree) << endl;
+}
+/*
+        1
+   2           2
+4    5      5     4
+
+*/
